beb57364d4f61844dd544dd0e08578d09319ea5f
Luidrocha <luidrocha@yahoo.com.br>
2022-01-19 14:44:58 -0300

    Delegate simples - Renorna o maior, o Quadrado e a Soma de 2 elementos

---
+++ Delegates_E_Operacoes_Lambda/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Delegates_E_Operacoes_Lambda.Services;
+
+namespace Delegates_E_Operacoes_Lambda
+{
+
+    delegate double BinaryNumericOperation(double n1, double n2); // Cria um delegate que recebe 2 argumentos
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+            // BinaryNumericOperation op = CalculationService.Sum;
+            BinaryNumericOperation op = new BinaryNumericOperation(CalculationService.Sum);
+            // double result = op(a, b);
+            double result = op.Invoke(a, b); // Funciona sem a função INVOKE
+            Console.WriteLine(result);
+        }
+    }
+
+}
+
